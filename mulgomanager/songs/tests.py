b84348b3bac9d771ccc92568795be27fcc38d7d2
kian <65789711+kianlin@users.noreply.github.com>
2021-06-18 13:16:47 +0800

    Add basic test cases for Song model

--- mulgomanager/songs/tests.py
+++ mulgomanager/songs/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from . models import Song
 
-# Create your tests here.
+
+class ModelTesting(TestCase):
+
+    def setUp(self):
+        self.songs = Song.objects.create(
+            title='testsong', artist='testartist', duration='00:03:25')
+
+    def test_song_model(self):
+        s = self.songs
+        self.assertTrue(isinstance(s, Song))
+
+    def test_song_title_return(self):
+        d = self.songs
+        self.assertEqual(str(d), 'testsong')
