27f76907ba79f52efc167f4985fa19aa6a01dcb1
kian <65789711+kianlin@users.noreply.github.com>
2021-06-25 15:39:01 +0800

    Improve code format

--- mulgomanager/lyrics/api.py
+++ mulgomanager/lyrics/api.py
@@ -1,9 +1,9 @@
 import os
-import requests
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, renderer_classes
 from rest_framework.renderers import JSONRenderer, BrowsableAPIRenderer
 import lyricsgenius
+# import requests
 
 
 headers = {
@@ -12,70 +12,57 @@ headers = {
 
 genius = lyricsgenius.Genius()
 
+
 @api_view(('GET', ))
 @renderer_classes((JSONRenderer, BrowsableAPIRenderer))
 def SearchAll(request):
-    
     payload = request.data
-    
     # url = "https://api.genius.com/search"
-
     # response = requests.request("GET", url, headers=headers, params=payload)
     # result = response.json()
-
     # title = request.data['field']
-    
     # Using lyricsgenius to retrieve scrapped lyrics
     song = genius.search_song(payload["search_term"])
     values = song.to_dict()
-    
     # Convert each line to a list element
-    lyrics = song.to_text().splitlines()
-    
+    # lyrics = song.to_text().splitlines()
     return Response(values)
 
 
+# REFERENCES
+"""
+Codes below are for reference
+For subsequent functionality implementation
+"""
 
 # Search for songs based on keywords
-
 # @api_view(('GET', ))
 # @renderer_classes((JSONRenderer, BrowsableAPIRenderer))
 # def SearchAll(request):
 #     url = "https://api.genius.com/search"
-
 #     payload=request.data
-    
 #     response = requests.request("GET", url, headers=headers, params=payload)
 #     result = response.json()
-    
 #     # Test whether can access the attributes of the response object
 #     title = []
 #     for song in result['response']['hits']:
 #         title.append(song['result']['title'])
-    
 #     song = genius.search_song(title[0])
 #     lyrics = song.to_text().splitlines() # Convert each line to a list element
-    
 #     # print(response.text)
 #     return Response(lyrics, status=response.status_code)
 
-
 # # Search for songs based on id
-
 # @api_view(('GET', ))
 # @renderer_classes((JSONRenderer, BrowsableAPIRenderer))
 # def SearchWithSongID(request):
 #     url = "https://api.genius.com/songs/378195"
-
 #     payload=request.data
-    
 #     response = requests.request("GET", url, headers=headers, params=payload)
 #     result = response.json()
-    
 #     # # Test whether can access the attributes of the response object
 #     # title = []
 #     # for song in result['response']['hits']:
 #     #     title.append(song['result']['title'])
-    
 #     # print(response.text)
 #     return Response(result, status=response.status_code)
--- mulgomanager/mulgomanager/settings.py
+++ mulgomanager/mulgomanager/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 """
 
 import os
-from decouple import config
 import environ
 
 
