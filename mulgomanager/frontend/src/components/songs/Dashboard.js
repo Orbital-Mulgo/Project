d292f4d6854dbae5489e34174b7ac09bcb79a73c
kian <65789711+kianlin@users.noreply.github.com>
2021-06-25 15:00:57 +0800

    Modify file name to Recorder.js

--- mulgomanager/frontend/src/components/songs/Dashboard.js
+++ mulgomanager/frontend/src/components/songs/Dashboard.js
@@ -1,12 +1,12 @@
 import React, { Fragment } from "react";
-import Form from "./Form";
+import Recorder from "./Recorder";
 import Songs from "./Songs";
-import MicRecorder from 'mic-recorder-to-mp3';
+import MicRecorder from "mic-recorder-to-mp3";
 
 export default function Dashboard() {
     return (
         <Fragment>
-            <Form />
+            <Recorder />
             <Songs />
         </Fragment>
     );
