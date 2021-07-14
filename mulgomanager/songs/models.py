391efeeec19286dcc284ecd79eb285c9d555653f
Marvin <marvin.leow999@gmail.com>
2021-07-14 17:31:51 +0800

    flake8

--- mulgomanager/songs/models.py
+++ mulgomanager/songs/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
- 
+
+
 class Song(models.Model):
     title = models.CharField(max_length=100)
     artist = models.CharField(max_length=100)
@@ -15,4 +16,3 @@ class Song(models.Model):
 
     def __str__(self):
         return self.title
-
