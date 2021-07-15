4e791dad2d889c8c3a29540443bca3f4be749372
kian <65789711+kianlin@users.noreply.github.com>
2021-07-16 04:28:54 +0800

    Update to MySQL

--- mulgomanager/mulgomanager/settings.py
+++ mulgomanager/mulgomanager/settings.py
@@ -93,9 +93,18 @@ WSGI_APPLICATION = 'mulgomanager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'mulgodatabase',
+        'USER': 'root',
+        'PASSWORD': 'MySQL12345',
+        # 'HOST': 'localhost',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    # }
 }
 
 
